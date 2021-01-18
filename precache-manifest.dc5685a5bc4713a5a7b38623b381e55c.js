dfd228401ceced865af3efe9e2bb91dc7d90bdfc
Crozarts <imarcrosby@optimum.net>
2021-01-18 18:59:53 -0500

    Updates

---
+++ precache-manifest.dc5685a5bc4713a5a7b38623b381e55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71bc82f6e4a2360f6aa0",
+    "url": "/reactPortfolio/static/css/main.4d846550.chunk.css"
+  },
+  {
+    "revision": "71bc82f6e4a2360f6aa0",
+    "url": "/reactPortfolio/static/js/main.71bc82f6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/reactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "3f2633917fc5b8f6a031a4c8eceaafa7",
+    "url": "/reactPortfolio/index.html"
+  }
+];
