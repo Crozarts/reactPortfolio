ac84b7a19c638baf3b9ae618ff6ac20d83ffde31
Crozarts <imarcrosby@optimum.net>
2021-01-18 17:46:54 -0500

    first

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Andrew Crosby",
+    "role": "Full Stack Developer and Graduate of Rutgers Bootcamp Fall 2020 Program ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Constantly evolving and constantly creating.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/crozarts",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I love to paint and attend live music events in my free time. Skateboarding, art, photography, and videography are just a few of my other passions. Technology definetely has always been an interest of mine as well which is why im excited to discover the many intricacies of being a Full Stack Developer. Constantly keeping myself busy by learning and growing in any way I can. I have two dogs, Hunter and Oso who keep me occupied. I prefer to be in the woods or deep into a craft, but I also love the city life. Excited to discover the next chapter of my life but also enjoying the ride along the way.",
+    "address":"United States",
+    "email":"crozarts@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Rutgers",
+        "specialization":"Full Stack Development Bootcamp",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
